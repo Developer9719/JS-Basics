3a373a3046064ddf85247420bb74df8bc3015180
Developer9719 <justin.bat19@gmail.com>
2025-08-23 23:10:54 -0400

    Variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables 
+// Declare and initializing the variable
+const myName = "Justin"; // const variables cant be reassigned
+let myAge = 28 // let variables can be reassigned
+
+myAge = 30; // Reassigning existing variable
+
+console.log(myName);
