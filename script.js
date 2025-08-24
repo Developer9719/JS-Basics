1b694c57dc84fd54de7d6bda748f4cff6f3d17e0
Developer9719 <justin.bat19@gmail.com>
2025-08-23 23:34:21 -0400

    Data Types and Conditionals Lesson

--- script.js
+++ script.js
@@ -6,3 +6,66 @@ let myAge = 28 // let variables can be reassigned
 myAge = 30; // Reassigning existing variable
 
 console.log(myName);
+
+// Number Data Type
+let n = 123;
+/**
+ * Integer: numbers without a decimal point
+ * Floating Point: numbers with a decimal point
+ * 
+ * Number Operations:
+ * - * (multiplication)
+ * - / (division)
+ * - + (addition)
+ * - - (subtraction)
+ *
+ * Special Numeric Values:
+ * - NaN (Not a Number)
+ *   - result of an invalid mathematical operation
+ * - Infinity
+ *   - value greater than any number 
+ * - Negative Infinity
+ */
+
+// String is a piece of text surrounded by quotes.
+let string = "Hello, World!";
+alert( `Hello, ${myName}!` ); // Hello, John!
+/**
+ * Double quotes are used for strings.
+ * Single quotes can also be used.
+ * Backticks are used for template literals. (alert)
+ */
+
+// Boolean only has 2 values: true and false.
+
+// Null value represents the intentional absence of any object value.
+
+// Undefined value represents a variable that has been declared but not assigned a value.
+
+// Object type are like variables but can store multiple values or complex entities.
+
+// Symbol types are unique and immutable values that can be used as object keys.
+
+// Typeof operator returns the type of a variable or expression.
+
+/** Conditionals
+ *  - if statement
+ *  - else statement
+ *  - else if statement
+ *  - switch statement
+ *    switch(expression) {
+        case x:
+            // code block
+            break;
+        case y:
+            // code block
+            break;
+        default:
+            // code block
+        }
+
+        The switch expression is evaluated once.
+        The value of the expression is compared with the values of each case.
+        If there is a match, the associated block of code is executed.
+        If there is no match, no code is executed.
+ */ 
